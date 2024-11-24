6f1efe2cf96222970414568795033107c856598e
biladiz <onuryag@gmail.com>
2024-11-24 12:41:36 +0000

    initial codebase
    initial gitignore from https://github.com/github/gitignore/tree/main/Global
    
    initial project setup from https://start.spring.io/

---
+++ src/test/java/com/tus/java/oop1/restaurant_management_system/RestaurantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.java.oop1.restaurant_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
